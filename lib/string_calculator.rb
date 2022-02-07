f0af98493605339d993f12286b529004582732f2
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 21:04:10 +0530

    Fix: Failing tests for newline characters in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,6 +7,10 @@ module StringCalculator
   def self.extract_values(string)
     # When converting empty string to a integer ruby by
     # default returns 0
-    string.split(',').map(&:to_i)
+    if string.include?("\n")
+      string.tr("\n", ',').split(',').map(&:to_i)
+    else
+      string.split(',').map(&:to_i)
+    end
   end
 end
