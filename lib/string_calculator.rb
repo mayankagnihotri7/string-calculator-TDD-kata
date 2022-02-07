6f0303f904bffc4de36fc55ef1c07ba574ca1b33
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 19:07:54 +0530

    Fix: Failing tests for two numbers in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,12 @@
 module StringCalculator
   def self.add(string)
+    values = extract_values(string)
+    values.sum
+  end
+
+  def self.extract_values(string)
     # When converting empty string to a integer ruby by
     # default returns 0
-    string.to_i
+    string.split(',').map(&:to_i)
   end
 end
