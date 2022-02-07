eeef164eac18a1c55f66c39eb87646174fde1f74
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 21:08:53 +0530

    Fix: Failing tests for delimiter in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,10 +7,6 @@ module StringCalculator
   def self.extract_values(string)
     # When converting empty string to a integer ruby by
     # default returns 0
-    if string.include?("\n")
-      string.tr("\n", ',').split(',').map(&:to_i)
-    else
-      string.split(',').map(&:to_i)
-    end
+    string.split(/\W+/).map(&:to_i)
   end
 end
