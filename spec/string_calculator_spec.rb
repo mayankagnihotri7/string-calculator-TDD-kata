d70995a5dd39ef3898bd60888cb0fcfbc8cc80a4
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 18:56:38 +0530

    Add failing test for single number in string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe StringCalculator do
     it 'returns 0 for empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it "returns 1 for '1'" do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
+
+    it "returns 2 for '2'" do
+      expect(StringCalculator.add('2')).to eq(2)
+    end
   end
 end
