8c5d185a9a4aa05284f5f3ecaf735b3f968db850
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 19:39:06 +0530

    Add failing tests for newline characters in string with numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -36,4 +36,30 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add('78,250')).to eq(328)
     end
   end
+
+  context 'newline character' do
+    it "returns 3 for the string '1\n,2'" do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
+
+    it "returns 6 for the string 1\n2, 3'" do
+      expect(StringCalculator.add('0,1')).to eq(6)
+    end
+
+    it "returns 6 for the string '1\n2\n3'" do
+      expect(StringCalculator.add('3,0')).to eq(6)
+    end
+
+    it "returns 8 for the string '3\n2\n3, 0'" do
+      expect(StringCalculator.add('190,10')).to eq(8)
+    end
+
+    it "returns 358 for the string '78\n250, 25\n25'" do
+      expect(StringCalculator.add('78,250')).to eq(358)
+    end
+
+    it "returns 428 for the string '78\n250, 25\n25', 50\n50" do
+      expect(StringCalculator.add('78,250')).to eq(428)
+    end
+  end
 end
