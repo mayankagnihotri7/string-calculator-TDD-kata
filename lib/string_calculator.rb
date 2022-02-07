794c841df7bbdc87c903456665153e827e38facc
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 23:14:33 +0530

    Fix: failing tests for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,6 +7,18 @@ module StringCalculator
   def self.extract_values(string)
     # When converting empty string to a integer ruby by
     # default returns 0
-    string.split(/\W+/).map(&:to_i)
+    num_array = string.split(/[^0-9-]+/).map(&:to_i)
+    return num_array unless check_for_negatives(num_array)
+  end
+
+  def self.check_for_negatives(array)
+    negatives = array.select(&:negative?)
+    return false if negatives.length == 0
+
+    raise_error_if_negative(negatives)
+  end
+
+  def self.raise_error_if_negative(negatives)
+    raise "negatives not allowed - (#{negatives.join(', ')})"
   end
 end
