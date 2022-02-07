0183ff5462ccabfa6125c89257a3ac24033bf12e
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 18:52:06 +0530

    Add Failing test for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,9 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  context 'single number' do
+    it 'returns 0 for empty string' do
+      expect(StringCalculator.add('')).to eq(0)
+    end
+  end
 end
