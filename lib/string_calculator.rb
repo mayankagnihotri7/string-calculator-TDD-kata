f5a9ceb58a4bdc0c9d5231707e69a5c8ba528aec
Mayank <mayankagnihotri7@gmail.com>
2022-02-07 23:30:41 +0530

    Refactor solution

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,24 +1,20 @@
 module StringCalculator
   def self.add(string)
     values = extract_values(string)
+    raise_error_if_negative_integer_present(values)
     values.sum
   end
 
   def self.extract_values(string)
     # When converting empty string to a integer ruby by
     # default returns 0
-    num_array = string.split(/[^0-9-]+/).map(&:to_i)
-    return num_array unless check_for_negatives(num_array)
+    string.split(/[^0-9-]+/).map(&:to_i)
   end
 
-  def self.check_for_negatives(array)
+  def self.raise_error_if_negative_integer_present(array)
     negatives = array.select(&:negative?)
     return false if negatives.length == 0
 
-    raise_error_if_negative(negatives)
-  end
-
-  def self.raise_error_if_negative(negatives)
     raise "negatives not allowed - (#{negatives.join(', ')})"
   end
 end
